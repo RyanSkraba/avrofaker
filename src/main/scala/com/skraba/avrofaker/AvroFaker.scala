f351a3ec467fae60451ff6f91d8d98c2258dd833
Ryan Skraba <ryan@skraba.com>
2026-02-14 11:53:17 +0100

    feat: Initial implementation of an Avro generator

--- src/main/scala/com/skraba/avrofaker/AvroFaker.scala
+++ src/main/scala/com/skraba/avrofaker/AvroFaker.scala
@@ -1,8 +1,89 @@
 package com.skraba.avrofaker
 
-import org.apache.avro.Schema;
+import org.apache.avro.Schema
+import org.apache.avro.Schema.Field
+import org.apache.avro.generic.{GenericRecord, GenericRecordBuilder}
 
-/** Generates Avro data. */
-class AvroFaker(spec: Schema) {
-  def generate(): Any = spec
+import scala.jdk.CollectionConverters.CollectionHasAsScala
+import scala.util.Random
+
+/** Given a schema, the AvroGenerator creates data that matches it. */
+sealed trait AvroFaker {
+  def generate(): Any;
+}
+
+object AvroFaker {
+  def apply(schema: Schema, rnd: Random = new Random()): AvroFaker = {
+    schema.getType match {
+      case Schema.Type.RECORD  => RecordGenerator(schema, rnd)
+      case Schema.Type.ENUM    => ???
+      case Schema.Type.ARRAY   => ???
+      case Schema.Type.MAP     => ???
+      case Schema.Type.UNION   => ???
+      case Schema.Type.FIXED   => ???
+      case Schema.Type.STRING  => StringGenerator(schema, rnd)
+      case Schema.Type.BYTES   => ???
+      case Schema.Type.INT     => IntGenerator(schema, rnd)
+      case Schema.Type.LONG    => ???
+      case Schema.Type.FLOAT   => ???
+      case Schema.Type.DOUBLE  => ???
+      case Schema.Type.BOOLEAN => ???
+      case Schema.Type.NULL    => NullGenerator(schema)
+    }
+  }
+}
+
+/** A RECORD schema generates field data according to the schema of its fields.
+  *
+  * @param schema
+  *   a schema of type RECORD
+  * @param rnd
+  *   random number generator (for reproducibility if desired)
+  */
+case class RecordGenerator(schema: Schema, rnd: Random = new Random()) extends AvroFaker {
+  assert(schema.getType == Schema.Type.RECORD, s"${Schema.Type.RECORD} required, ${schema.getType} found")
+  val fieldGenerators: Map[Field, AvroFaker] =
+    schema.getFields.asScala.map((f: Field) => f -> AvroFaker(f.schema(), rnd)).toMap
+  def generate(): GenericRecord = {
+    val rb = new GenericRecordBuilder(schema)
+    fieldGenerators.map { case (f, fgen) => rb.set(f, fgen.generate()) }
+    rb.build()
+  }
+}
+
+/** A STRING schema generates a 10 character string.
+  *
+  * @param schema
+  *   a schema of type STRING
+  * @param rnd
+  *   random number generator (for reproducibility if desired)
+  */
+case class StringGenerator(schema: Schema, rnd: Random = new Random()) extends AvroFaker {
+  assert(schema.getType == Schema.Type.STRING, s"${Schema.Type.STRING} required, ${schema.getType} found")
+  def generate(): String = rnd.alphanumeric.take(10).mkString
+}
+
+/** An INT schema generates an increasing sequence starting from zero.
+  *
+  * @param schema
+  *   a schema of type INT
+  * @param rnd
+  *   random number generator (for reproducibility if desired)
+  */
+case class IntGenerator(schema: Schema, rnd: Random = new Random()) extends AvroFaker {
+  assert(schema.getType == Schema.Type.INT, s"${Schema.Type.INT} required, ${schema.getType} found")
+  var seq: Int = -1
+  def generate(): Int = { seq += 1; seq }
+}
+
+/** A NULL schema generates only null.
+  *
+  * @param schema
+  *   a schema of type NULL
+  * @param rnd
+  *   random number generator (for reproducibility if desired)
+  */
+case class NullGenerator(schema: Schema) extends AvroFaker {
+  assert(schema.getType == Schema.Type.NULL, s"${Schema.Type.NULL} required, ${schema.getType} found")
+  def generate(): Any = null
 }
--- src/test/scala/com/skraba/avrofaker/AvroFakerSpec.scala
+++ src/test/scala/com/skraba/avrofaker/AvroFakerSpec.scala
@@ -1,7 +1,50 @@
 package com.skraba.avrofaker
 
+import org.apache.avro.generic.GenericRecordBuilder
+import org.apache.avro.{Schema, SchemaBuilder}
 import org.scalatest.funspec.AnyFunSpecLike
 import org.scalatest.matchers.should.Matchers
 
-/** Unit tests for [[AvroFaker]]. */
-class AvroFakerSpec extends AnyFunSpecLike with Matchers {}
+import scala.util.Random
+
+class AvroFakerSpec extends AnyFunSpecLike with Matchers {
+  // RECORD, ENUM, ARRAY, MAP, UNION, FIXED, STRING, BYTES, INT, LONG, FLOAT, DOUBLE, BOOLEAN, NULL
+
+  describe("Generating Avro RECORD data") {
+    it("should create ten character strings by default") {
+      val schema = SchemaBuilder.record("Example").fields().requiredInt("id").requiredString("name").endRecord();
+      val gen = AvroFaker(schema, new Random(0L))
+      gen.generate() shouldBe new GenericRecordBuilder(schema).set("id", 0).set("name", "CCzLNHBFHu").build()
+      gen.generate() shouldBe new GenericRecordBuilder(schema).set("id", 1).set("name", "RvbI1iI19W").build()
+      gen.generate() shouldBe new GenericRecordBuilder(schema).set("id", 2).set("name", "jGGR8UNWut").build()
+    }
+  }
+
+  describe("Generating Avro STRING data") {
+    it("should create ten character strings by default") {
+      val gen = AvroFaker(Schema.create(Schema.Type.STRING), new Random(0L))
+      gen.generate() shouldBe "CCzLNHBFHu"
+      gen.generate() shouldBe "RvbI1iI19W"
+      gen.generate() shouldBe "jGGR8UNWut"
+    }
+  }
+
+  describe("Generating Avro INT data") {
+    it("should create a sequence by default") {
+      val gen = AvroFaker(Schema.create(Schema.Type.INT), new Random(0L))
+      gen.generate() shouldBe 0
+      gen.generate() shouldBe 1
+      gen.generate() shouldBe 2
+    }
+  }
+
+  describe("Generating Avro NULL data") {
+    it("should only ever generate NULL") {
+      val gen = AvroFaker(Schema.create(Schema.Type.NULL), new Random(0L))
+      Option(gen.generate()) shouldBe None
+      Option(gen.generate()) shouldBe None
+      Option(gen.generate()) shouldBe None
+    }
+  }
+
+}
